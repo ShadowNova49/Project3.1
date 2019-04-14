157e611817957b7fcbf502dd91ba0c4c99666d27
Nikita <noshalygin@gmail.com>
2019-04-15 00:32:01 +0300

    11.04

---
+++ task3.1/ImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCell.swift
+//  task3.1
+//
+//  Created by Nikita on 15.04.2019.
+//  Copyright © 2019 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    
+}
--- task3.1/ViewController.swift
+++ task3.1/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let layout = collectionView.collectionViewLayout as? PinterestLayout {
+            layout.delegate = self
+        }
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+
     }
-
-
 }
 
+extension ViewController: PinterestLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+        /*
+        let image = viewModel.cellViewModels[indexPath.item].image
+        let height = image.size.height
+        
+        return height
+        */
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ImageCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return 10
+    }
+
+}
