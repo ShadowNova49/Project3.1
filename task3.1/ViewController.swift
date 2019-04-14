327e5c9fb6d4d094b024f6c98b5f720a2b668ca3
Nikita <noshalygin@gmail.com>
2019-04-14 23:50:09 +0300

    Initial Commit

---
+++ task3.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task3.1
+//
+//  Created by Nikita on 14.04.2019.
+//  Copyright © 2019 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
